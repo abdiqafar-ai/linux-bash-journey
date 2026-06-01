7e922ac8ad97ffc2b48ff02c0c70d3f5d92a00bd
abdiqafar-ai <iabdiqafar24@gmail.com>
2026-06-01 19:36:37 +0300

    Well practiced bash-scripting loops

---
+++ arrays.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# servers=("web" "db" "cache" "auth")
+# servers+=("landing")
+
+# read -p "Server name: " server
+# servers+=("$server")
+
+# for server in "${servers[@]}"
+# do
+#     if [ "$server" = "db" ]
+#     then
+#         echo "$server requires backup"
+#     else
+#         echo "$server healthy"
+#     fi
+# done
+
+# servers=("web" "db" "cache" "auth")
+
+# for server in "${servers[@]}"
+# do
+#     check "$server"
+# done
+
+servers=("web" "db" "cache" "auth" "monitoring")
+
+for server in "${servers[@]}"
+do 
+    echo "checking $server"
+done
